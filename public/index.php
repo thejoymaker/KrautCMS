cf5180e0e4799b84fe9fb945f2dc8d9cadef8304
Happy Joy <thejoymaker@pm.me>
2024-12-20 10:15:44 +0100

    catch uncaught exception around everything

--- public/index.php
+++ public/index.php
@@ -14,43 +14,48 @@ require __DIR__ . '/../vendor/autoload.php';
 use Kraut\Kernel;
 use Dotenv\Dotenv;
 
-// Load environment variables from .env file
-function initializeDotenv()
-{
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->safeLoad();
-}
-initializeDotenv();
-if (isset($_ENV['APP_DEBUG']) === false) {
-    echo "Failed to load environment variables.";
+try {
+    // Load environment variables from .env file
+    function initializeDotenv()
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->safeLoad();
+    }
+    initializeDotenv();
+    if (isset($_ENV['APP_DEBUG']) === false) {
+        echo "Failed to load environment variables.";
+        die();
+    }
+    if ($_ENV['APP_DEBUG'] === 'true') {
+        enableErrorReporting();
+    }
+    /**
+     * Main entry point for the application.
+     *
+     * This function initializes the application kernel, handles the incoming HTTP request,
+     * and sends the appropriate HTTP response.
+     *
+     * @return void
+     */
+    function main()
+    {
+        $kernel = new Kernel();
+        $response = $kernel->handle(
+            $_SERVER['REQUEST_METHOD'],
+            $_SERVER['REQUEST_URI']
+        );
+        // Send HTTP headers and response
+        http_response_code($response->getStatusCode());
+        foreach ($response->getHeaders() as $name => $values) {
+            foreach ($values as $value) {
+                header(sprintf('%s: %s', $name, $value), false);
+            }
+        }
+        echo $response->getBody();
+    }
+    main();
+} catch (Throwable $e) {
+    echo "No sir, I don't like it.";
+    // TODO log...
     die();
 }
-if ($_ENV['APP_DEBUG'] === 'true') {
-    enableErrorReporting();
-}
-/**
- * Main entry point for the application.
- *
- * This function initializes the application kernel, handles the incoming HTTP request,
- * and sends the appropriate HTTP response.
- *
- * @return void
- */
-function main()
-{
-    $kernel = new Kernel();
-    $response = $kernel->handle(
-        $_SERVER['REQUEST_METHOD'],
-        $_SERVER['REQUEST_URI']
-    );
-    // Send HTTP headers and response
-    http_response_code($response->getStatusCode());
-    foreach ($response->getHeaders() as $name => $values) {
-        foreach ($values as $value) {
-            header(sprintf('%s: %s', $name, $value), false);
-        }
-    }
-    echo $response->getBody();
-}
-main();
-?>
