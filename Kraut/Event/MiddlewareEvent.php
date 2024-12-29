baff8830b717b30aacece51e5ed53e69c25ae152
Happy Joy <thejoymaker@pm.me>
2024-12-29 07:47:41 +0100

    Refactor postProcess method to improve middleware handling and add detailed documentation

--- Kraut/Event/MiddlewareEvent.php
+++ Kraut/Event/MiddlewareEvent.php
@@ -17,13 +17,26 @@ class MiddlewareEvent extends Event
         $this->middlewareQueue = $middlewareQueue;
     }
 
+
+    /**
+     * Post-processes the middleware queue by adding new middleware in relation to existing ones.
+     *
+     * This method loops through the post-processing queue and adds new middleware based on their
+     * specified relation ('before' or 'after') to existing middleware. It continues to loop until
+     * no more new middleware can be added or the maximum number of iterations is reached.
+     *
+     * @return void
+     */
     public function postProcess(): void
     {
-        $maxRepeat = sizeof($this->middlewareAdded);
+        $maxRepeat = count($this->middlewareAdded);
         $numberAdded = 0;
+        // loop through the postprocessing queue as long as there are new middleware to add
         for ($i = 0; $i < $maxRepeat; $i++) {
             foreach ($this->middlewareAdded as $newMiddleware => $targetMiddleware) {
                 foreach ($targetMiddleware as $middleware => $relation) {
+                    // check if the new middleware is not already present in 
+                    // the queue and the target middleware is present
                     if(!$this->isPresent($newMiddleware) && $this->isPresent($middleware)) {
                         switch ($relation) {
                             case 'before':
@@ -37,11 +50,10 @@ class MiddlewareEvent extends Event
                     }
                 }
             }
-            if($numberAdded === $maxRepeat) {
+            if ($numberAdded === $maxRepeat) {
                 break;
             }
         }
-
     }
 
     public function getMiddlewareQueue(): array
